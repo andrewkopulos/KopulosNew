ca30ca4dfcba2e3fb4a190b85866e249dfab4f8d
andrewkopulos <andrewkopulos@Andrews-MacBook-Air-2.local>
2019-02-21 11:55:34 -0600

    Replace

---
+++ GameOfCircles/Enemy.py
@@ -0,0 +1,13 @@
+from Sprite import Sprite
+
+class Enemy(Sprite):
+    
+    speed = 3
+    diameter = 50
+    c = color(0,0,255)
+    
+    def move(self):
+        self.x += self.speed
+        if self.x < 0 or self.x > width:
+            self.speed *= -1
+        
